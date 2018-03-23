ccde59068e22ffc5e5514cab51af11965386a9e3
andrew-watt <36197314+andrew-watt@users.noreply.github.com>
2018-03-23 14:13:44 +0000

    adding main pages

---
+++ src/header.php
@@ -0,0 +1,7 @@
+<div class="section header">
+  <div class="nav">
+    <div class="nav-home"><a href="#">Home</a></div>
+    <!-- Need PHP here to get the username -->
+    <div class="nav-user">Hello, <!--user-->. Not you? <a href="login.php">Sign Out</a></div>
+  </div>
+</div>
