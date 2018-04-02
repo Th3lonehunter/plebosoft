0615380f774e45e2631cbd6e01d975eeaf15481d
williamhutchings14 <williamhutchings14@gmail.com>
2018-04-02 14:47:47 -0400

    message

--- src/Option_code.php
+++ src/Option_code.php
@@ -8,15 +8,15 @@ include_once("../src/php/DBConnect.php");
 
 <?php
 
-        $sql = "SELECT * FROM Plebosoft_Departments";
+        $sql = "SELECT * FROM Plebosoft_Categories";
             $res = mysqli_query($dblink,$sql) or die(mysqli_error());
-?>
-<form action="../src/php/IdeaTagParse.php" method="POST">
-  
 
+echo "<form action='../src/php/IdeaTagParse.php?id={$_GET['id']}' method='POST'>";
+  
+?>
 <select name="Select-tag">
   <?php while($row = mysqli_fetch_array($res)):;  ?>
-  <option value="<?php echo $row['departmentID'];?>"><?phpecho $row['name']; ?></option> 
+  <option value="<?php echo $row['CategoryID'];?>"><?php echo $row['name']; ?></option> 
   <?php endwhile;  ?>
   <input type='submit' name='tagssub' value='Add Tag'>
   <input type='submit' name='tagssub' value='Add Tag and go to Home'>
--- src/php/IdeaTagParse.php
+++ src/php/IdeaTagParse.php
@@ -5,49 +5,37 @@ include_once("DBConnect.php");
 session_start();
 
 if (isset($_POST['tagssub'])){
-    if($_POST['tagssub'] == "add another"){
+    if($_POST['tagssub'] == "Add Tag"){
  $tag="";
  $tag.=$_POST['Select-tag'];
+        if(isset($_POST['Select-tag'])){
     
-    $sql="SELECT * FROM Plebosoft_Categories WHERE name='".$tag."'";
-    $res = mysqli_query($dblink,$sql) or die(mysqli_error());
-    if(mysqli_num_rows($res)==1){
-    $row = mysqli_fetch_assoc($res);
-        $ntag = $row['CategoryID'];
     
-  $IDeaID = $_GET['IdeaID'];
+    
+  $IDeaID = $_GET['id'];
   $UID =$_SESSION['uid'];
-  $IDeaID = stripcslashes($IDeaID);
-  $IDeaID = strip_tags($IDeaID);
-  $IDeaID = mysqli_real_escape_string($dblink, $IDeaID);
+
 
 
     
-    $sql1 = " INSERT INTO Plebosoft_Ideas_Categories (ideaID, categoryID, userID) VALUES('".$IDeaID."', '".$ntag."', '".$UID."')"
+    $sql1 = " INSERT INTO Plebosoft_Ideas_Categories (ideaID, categoryID, userID) VALUES('".$IDeaID."', '".$tag."', '".$UID."')";
         $res1 = mysqli_query($dblink, $sql1) or die('Error: '.mysqli_error($dblink));
         if(($res1)){
-            header("Location: ");
+            echo $_POST['Select-tag'];
+            header("Location: ../Option_code.php?id={$IDeaID}");
         }else{
             echo " There was a problem with the creation of you user Post plase try again or contact suport staff";
             }
-   
-}
-else{
-    header("Location: ");
-}
+        }else{echo "Failed to get catagory";}
+
  
-    }else if($_POST['tagssub'] == "Post Idea"){
+    }else if($_POST['tagssub'] == "Add Tag and go to Home"){
         
         $tag="";
         $tag.=$_POST['Select-tag'];
     
-        $sql="SELECT * FROM Plebosoft_Categories WHERE name='".$tag."'";
-        $res = mysqli_query($dblink,$sql) or die(mysqli_error());
-        if(mysqli_num_rows($res)==1){
-        $row = mysqli_fetch_assoc($res);
-        $ntag = $row['CategoryID'];
-    
-            $IDeaID = $_GET['IdeaID'];
+        
+            $IDeaID = $_GET['id'];
             $UID =$_SESSION['uid'];
             $IDeaID = stripcslashes($IDeaID);
             $IDeaID = strip_tags($IDeaID);
@@ -55,22 +43,20 @@ else{
 
 
     
-        $sql1 = " INSERT INTO Plebosoft_Ideas_Categories (ideaID, categoryID, userID) VALUES('".$IDeaID."', '".$ntag."', '".$UID."')"
+        $sql1 = " INSERT INTO Plebosoft_Ideas_Categories (ideaID, categoryID, userID) VALUES('".$IDeaID."', '".$tag."', '".$UID."')";
         $res1 = mysqli_query($dblink, $sql1) or die('Error: '.mysqli_error($dblink));
         if(($res1)){
-            header("Location: ");
+            header("Location: ../home.php");
         }else{
             echo " There was a problem with the creation of you user Post plase try again or contact suport staff";
             }
    
-}else{
-    header("Location: ");
-}
+
     }
     
     
 }else{
-    header("Location: ");
+    header("Location: ../home.php?Post_failed");
 }
 
 ?>
